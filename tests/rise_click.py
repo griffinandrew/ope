7d9b55885deece4b65b46596ffb5e5bfe65662de
Griffin Heyrich <76177337+griffinandrew@users.noreply.github.com>
2023-06-09 16:57:25 -0400

    update chrome do to vers issue

--- tests/rise_click.py
+++ tests/rise_click.py
@@ -1,4 +1,5 @@
-from selenium import webdriver
+f
+om selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
@@ -6,6 +7,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import platform
 import sys
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 if len(sys.argv) <= 1:
     print("Invalid number of arguments")
